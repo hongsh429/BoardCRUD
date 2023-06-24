9832ac42485fbded83611409ae5a1be873b41a43
hongsh429 <shh0429@kakao.com>
2023-06-24 18:35:27 +0900

    first commit

---
+++ src/test/java/firstweek/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package firstweek.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
